3ec325d34fdb6b30140df8b627955ac109668558
ritssoft <63286559+ritssoft@users.noreply.github.com>
2022-04-18 08:18:18 +0530

    Jenkins-pipeline1

---
+++ jenkins-pipeline/Instance/variable.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {}
+variable "az" {
+    description = "Availibility Zone"  
+    #type = list(string)
+}
+variable "env-prefix" {}
+variable "public_key_location" {}
+variable "subnet_id" {}
+variable "key_name" {}
+variable "vm_name" {}
+
